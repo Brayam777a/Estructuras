85ee9afd189381c219b3eb453ece2ad105641080
Sala K 405 <salak405@salasfr.itm.edu.co>
2026-02-09 09:18:38 -0500

    Suma

--- nachoele.java
+++ nachoele.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
 public class nachoele {
     public static void main(String[] args) {
-        System.out.println("hola mundo");
+        System.out.println("Vamos a sumar");
+        int n1=0, n2=0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ingrese el numero uno");
+        n1=sc.nextInt();
+        System.out.println("ingrese el numero dos");
+        n2=sc.nextInt();
+        System.out.println("la suma de los dos numeros es "+ (n1+n2));
+
+
     }
     
 
